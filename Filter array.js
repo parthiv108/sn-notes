32cb8abbb69da7744a2d5411e177985e0fc053c6
parthiv108 <parthiv108@gmail.com>
2022-07-26 15:15:21 +1000

    Update Filter array.js

--- Filter array.js
+++ Filter array.js
@@ -1,4 +1,5 @@
 //filter out the approved timesheets(timesheetArr) users from the userRoleArr to get the user who need to be be sent the mail
+//this removes the common elements btn the 2 arrays, to give you the elements present in the superset array (userRoleArr)
 var filtered = userRoleArr.filter(
     function (e) {
         return this.indexOf(e) < 0; //returns true if element e (in userRoleArr) doesn't exist in the timesheetArr ('this' refers to timesheetArr passed as a param)
